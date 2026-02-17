621e30f4b3ae5c3bb10cdbf93901cc8061251046
nabil-hajali <nabil2491@gmail.com>
2026-02-17 16:23:46 +0100

    print the bike with the less weight

--- Snack1/main.js
+++ Snack1/main.js
@@ -6,28 +6,53 @@ const bici = [
     // describe name and weight of the bikes
     {
         nome: 'Bianchi',
-        peso: '6.6',
+        peso: 6.6,
     },
     {
         nome: 'Colnago',
-        peso: '6.5',
+        peso: 6.5,
     },
     {
         nome: 'Pinarello',
-        peso: '7.35',
+        peso: 7.35,
     },
     {
         nome: 'Atala',
-        peso: '18',
+        peso: 18,
     }
 ]
 //initialize a for condition
+//sostituisce con il peso minore
+let pesoMinore = bici[0].peso;
 for (let i = 0; i < bici.length; i++) {
-    const pesoBici = bici[i];
-    console.log(pesoBici.peso);
+    const pesoObg = bici[i];
+    console.log(pesoObg);
     
+    if(pesoMinore > pesoObg.peso){
+        pesoMinore = pesoObg.peso
+    }
     
 }
+
+console.log(pesoMinore);
+
+    // sostituisce con il peso maggiore
+ /*    let pesoMinore = bici[0].peso;
+for (let i = 0; i < bici.length; i++) {
+    const pesoObg = bici[i];
+    console.log(pesoObg);
+    
+    if(pesoMinore < pesoObg.peso){
+        pesoMinore = pesoObg.peso
+    }
+    
+}
+    
+    console.log(pesoMinore);
+     */
+
+//console.log(pesoBiciInferiore);
+
 
 
 // Stampare a schermo la bici con peso minore.
