63ccde945cab417b59da3dafb3575a19b0075769
nabil-hajali <nabil2491@gmail.com>
2026-02-17 13:23:53 +0100

    for cycle to get all the weight

--- Snack1/main.js
+++ Snack1/main.js
@@ -6,21 +6,28 @@ const bici = [
     // describe name and weight of the bikes
     {
         nome: 'Bianchi',
-        peso: '6.6'
+        peso: '6.6',
     },
     {
-        nome: 'Colnago'
-        peso: '6.5'
+        nome: 'Colnago',
+        peso: '6.5',
     },
     {
-        nome: 'Pinarello'
-        peso: '7.35'
+        nome: 'Pinarello',
+        peso: '7.35',
     },
     {
-        nome: 'Atala'
-        peso: '18'
+        nome: 'Atala',
+        peso: '18',
     }
 ]
+//initialize a for condition
+for (let i = 0; i < bici.length; i++) {
+    const pesoBici = bici[i];
+    console.log(pesoBici.peso);
+    
+    
+}
 
 
 // Stampare a schermo la bici con peso minore.
