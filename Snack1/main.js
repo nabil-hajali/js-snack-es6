c25cc22e1ab70e4ff18ae56481c5a61177bb95bd
nabil-hajali <nabil2491@gmail.com>
2026-02-17 13:19:27 +0100

    create and objects arrow and describe name and weight

---
+++ Snack1/main.js
@@ -0,0 +1,26 @@
+
+// Creare un array di oggetti:
+//create an array with objects
+const bici = [
+    // Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso.
+    // describe name and weight of the bikes
+    {
+        nome: 'Bianchi',
+        peso: '6.6'
+    },
+    {
+        nome: 'Colnago'
+        peso: '6.5'
+    },
+    {
+        nome: 'Pinarello'
+        peso: '7.35'
+    },
+    {
+        nome: 'Atala'
+        peso: '18'
+    }
+]
+
+
+// Stampare a schermo la bici con peso minore.
