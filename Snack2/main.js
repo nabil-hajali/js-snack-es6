10d95c7ff4dc881539dfee3c3afbe90f67ddea85
nabil-hajali <nabil2491@gmail.com>
2026-02-18 09:59:46 +0100

    create the squad arry

---
+++ Snack2/main.js
@@ -0,0 +1,35 @@
+// Creare un array di oggetti di squadre di calcio.
+// Ogni squadra avrà diverse proprietà: nome, punti fatti, falli subiti.
+// Nome sarà l’unica proprietà da compilare, le altre saranno tutte settate a 0.
+const teams = [
+    {
+        name: 'Milan',
+        points: 0,
+        fouls: 0
+    },
+    {
+        name: 'Inter',
+        points: 0,
+        fouls: 0
+    },
+    {
+        name: 'Juve',
+        points: 0,
+        fouls: 0
+    },
+    {
+        name: 'Hellas',
+        points: 0,
+        fouls: 0
+    },
+]
+
+
+
+
+
+
+// Generare numeri random al posto degli 0 nelle proprietà “punti” fatti e “falli subiti”.
+
+
+// Infine, creiamo un nuovo array i cui elementi contengono solo nomi e falli subiti e stampiamo tutto in console
